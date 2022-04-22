b92572e7aff99342520fd6cdf7fab11c351d02c6
suraksha <surakshakumari.bcsf18@iba-suk.edu.pk>
2022-04-22 14:54:50 +0500

    commited

---
+++ index.js
@@ -0,0 +1,33 @@
+// const http = require('http')
+// const fs = require('fs')
+// const server = http.createServer(function(req, res){
+//     if(req.url == '/'){ 
+//         //res.write('homepage')
+//         const homepage = fs.readFileSync('index.html')
+//         res.end(homepage)
+// }
+//     else if(req.url == '/about') {
+//         //res.write('about')
+//    const about= fs.readFileSync('about.html')
+//     res.end(about)
+//     }
+//     })
+
+//     server.listen(3000, function
+//     (){
+//     console.log(" port 3000")})
+
+const express = require('express')
+
+const app = express()
+
+app.get('/', function(req, res) {
+    res.json({msg:'success'})
+})
+
+app.get('/about', function(req, res) {
+    res.json({data:'available' , msg:'success'})
+})
+
+app.listen(3000, function(){
+         console.log(" listening port 3000")})
