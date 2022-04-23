c580b0c3d6534f2b8927209aca5a2fbe7dae38ed
suraksha <surakshakumari.bcsf18@iba-suk.edu.pk>
2022-04-23 15:22:30 +0500

    css working

--- index.js
+++ index.js
@@ -37,12 +37,16 @@
 
 //................................................for area
 
-var area = require('./area');
-// var measure = 
-// { area: (x,y) =>  (x*y)
-// };
+// var area = require('./area');
+// // var measure = 
+// // { area: (x,y) =>  (x*y)
+// // };
 
- function solve(x,y){
-     console.log("area is: " + area.area(x,y))
-}
-solve(2,3);
+//  function solve(x,y){
+//      console.log("area is: " + area.area(x,y))
+// }
+// solve(2,3);
+
+
+//.................................................
+ok
