6ba1eac487cc39c733302c8a36bc07bb7141b754
Suraksha <surakshak571@gmail.com>
2022-04-26 00:01:07 +0500

    css with html

--- index.js
+++ index.js
@@ -1,75 +1,25 @@
-// const http = require('http')
-// const fs = require('fs')
-// const server = http.createServer(function(req, res){
-//     if(req.url == '/'){ 
-//         //res.write('homepage')
-//         const homepage = fs.readFileSync('index.html')
-//         res.end(homepage)
-// }
-//     else if(req.url == '/about') {
-//         //res.write('about')
-//    const about= fs.readFileSync('about.html')
-//     res.end(about)
-//     }
-//     })
 
-//const area = require("./area")
+ const express = require('express');
+ const path= require('path');
+const app = express()
 
-//     server.listen(3000, function
-//     (){
-//     console.log(" port 3000")})
-//...............................................................
+app.use(express.static('public'))
 
-// const express = require('express')
+app.get('/', function(req,res){
+    const homepage = path.resolve(__dirname, 'index.html')
+    //console.log(__dirname);
+    res.sendFile(homepage);
 
-// const app = express()
+})
 
-// app.get('/', function(req, res) {
-//     res.json({msg:'success'})
-// })
+app.all('/about', function(req,res){
+    const aboutpage = path.resolve(__dirname, 'about.html')
+    console.log(__dirname);
+    res.sendFile(aboutpage);
 
-// app.get('/about', function(req, res) {
-//     res.json({data:'available' , msg:'success'})
-// })
-
-// app.listen(3000, function(){
-//          console.log(" listening port 3000")})
+})
+app.listen(3000, function(){
+    console.log('listening to 3000 port');
+})
 
 
-
-//................................................for area
-
-// var area = require('./area');
-// // var measure = 
-// // { area: (x,y) =>  (x*y)
-// // };
-
-//  function solve(x,y){
-//      console.log("area is: " + area.area(x,y))
-// }
-// solve(2,3);
-
-
-//................................................. for callback function area
-
-// function solve(x,y){
-//     area(x,y,(err, area) => {
-//     if(err){console.log("error" +err.m)}
-//     else{ console.log("area is " +  area.area());}
-//      });
-//      console.log("after arrea");
-//     }
-//     solve(0,3);
-
-
-//..............................................
-const http = require('http')
-const fs = require('fs')
-const path = require('path')
-
-const server = http.createServer((req,res) => {
-res.write('request for ' + req.url+ "method is "+ req.method)
-
-res.end()
-}) ;
-server.listen(3000, function(req,res){console.log("listen this port")});
